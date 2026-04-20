48498127e6f42e72082638b7c7af0eb4c4b89128
yousefHalsayegh <96050534+yousefHalsayegh@users.noreply.github.com>
2026-04-20 10:20:30 +0100

    starting code

---
+++ src/isaac_so_arm101/robots/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2024-2025, Muammer Bay (LycheeAI), Louis Le Lay
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from .trs_so100.so_arm100 import *
+from .trs_so101.so_arm101 import *
