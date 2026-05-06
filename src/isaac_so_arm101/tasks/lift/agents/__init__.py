654bfb5bd305ee71c8d750039b8c8c9a91e06275
Yousef <yousef.h.alsayegh@gmail.com>
2026-05-06 19:01:39 +0100

    starting to refract the code and to implement a way to create dataset using the sim

--- src/isaac_so_arm101/tasks/lift/agents/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024-2025, Muammer Bay (LycheeAI), Louis Le Lay
-# All rights reserved.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-#
-# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
-# All rights reserved.
-#
-# SPDX-License-Identifier: BSD-3-Clause
