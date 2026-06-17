6dc085c8c44cb42416a68777d9356dff8c28332b
yousefHalsayegh <96050534+yousefHalsayegh@users.noreply.github.com>
2026-06-17 02:39:31 +0100

    speed test

--- src/ale/game_rl.py
+++ src/ale/game_rl.py
@@ -24,14 +24,14 @@ def format_time(seconds):
 
 
 def main():
-    env = gym.make("ALE/Pong-v5", frameskip=4, render_mode = "human")
+    env = gym.make("ALE/Pong-v5", frameskip=4)
     env.reset(seed=42)
     frame = Frames()
     brain = Brain()
     steps = 0
     start_time = time.time()
     episode_time = []
-    path = "checkpoint/brain4900.pth"
+    path = ""
     if os.path.exists(path):
         steps, start = brain.load_checkpoint(path)
         print("loading... ", path )
@@ -50,12 +50,13 @@ def main():
             goal_reward = 0
             tracking_reward = 0 
             prev_paddle_y = None
-
-
+            step_time = []
+            ep_step = 0 
             while True:
-
+                
+                start_eta = time.time()
                 action = brain.predict_next_action(state, steps, env)
-                obs, raw_reward, terminated, truncated, _ = env.step(action)
+                obs, raw_reward, terminated, truncated, _ = env.step(0)
                 reward = 0
                 
                 done = terminated or truncated
@@ -94,21 +95,23 @@ def main():
                 state = next_state
                 total_reward += reward
                 steps += 1 
-
+                ep_step += 1
                 loss = brain.train()
                 if loss == None : 
                     loss = 0
-
+                
+                step_time.append(time.time() - start_eta)
                 if done:
                     break
             ep_time = time.time() - ep
             episode_time.append(ep_time)
             eta = np.mean(episode_time[-100:]) * (config.EPISODES - episode - 1)
-            print(f"Episode {episode} | Steps {steps} | Total Reward {total_reward:.1f} | Tracking Reward {tracking_reward:.1f} |  Goal Reward {goal_reward:.1f} (Actual {(goal_reward/config.GOAL_REWARD):.1f})| Loss {loss:.5f}")
-            print(f"Episode time {format_time(ep_time)} | Total time {format_time(time.time() - start_time)} | ETA {format_time(eta)}")
+            print(f"Episode {episode} | Episode Steps {ep_step} | Total Steps {steps}|  Loss {loss:.5f}")
+            print(f"Total Reward {total_reward:.1f} | Tracking Reward {tracking_reward:.1f} |  Goal Reward {goal_reward:.1f} (Actual {(goal_reward/config.GOAL_REWARD):.1f})")
+            print(f"Episode time {format_time(ep_time)} |Mean Step time {np.mean(step_time):.3f} | Total time {format_time(time.time() - start_time)} | ETA {format_time(eta)}")
         
             if episode % config.MID_SAVE == 0 and episode != 0: 
-                brain.save_checkpoint(episode, steps)
+                brain.save_checkpoint(episode, steps, total_reward, tracking_reward, goal_reward, loss,"rob")
             if episode % config.FULL_SAVE == 0 and episode != 0: 
                 brain.save()
 
--- src/ale/test.py
+++ src/ale/test.py
@@ -1,42 +1,6 @@
-import time
-import os
-import json
-from robot_noVla import Robot
-import threading
+import gymnasium as gym
+import ale_py
 
-def measure_execution_time(robot, task):
-    t_start = time.perf_counter()
-    robot.rb.send_action(robot.positions[task])
-    start = time.perf_counter()
-    while (time.perf_counter() - start) < 10:
-        obs   = robot.rb.get_observation()
-        error = max(
-            abs(obs.get(k, 0) - robot.positions[task][k])
-            for k in robot.positions[task]
-        )
-        if error < 5 and (task == "neutral" or task =="home") :
-            break
-        if robot.action != 0 :
-            robot.action = 0 
-            print("did the action")
-            break
-        
-    
-    elapsed = time.perf_counter() - t_start
-    print(f"Execution time: {elapsed:.3f}s")
-
-
-robot = Robot()
-
-if os.path.exists("positions.json"):
-    with open("positions.json", "r") as f:
-        temp = json.load(f)
-    robot.positions = {keys:values for keys, values in temp.items()}
-
-robot.start()
-controller = threading.Thread(target=robot.controller, daemon=True)
-controller.start()
-
-for i in ['home', 'neutral', 'up', 'neutral', 'down', 'up', 'down', 'neutral', 'home']:
-    print("doing ", i)
-    measure_execution_time(robot, i)
+gym.register_envs(ale_py)
+env = gym.make("ALE/Pong-v5", frameskip=4, render_mode = "human")
+env.reset(seed=42)
